7c65f91781a1124a931001c9799c5ecdce259314
Abe Alappat <149266163+abemdxb@users.noreply.github.com>
2023-12-08 15:35:08 -0500

    fix overwrite issue

--- multiple_chunk_strategy.py
+++ multiple_chunk_strategy.py
@@ -118,10 +118,9 @@ def save_dataframe_to_parquet(dataframe: pd.DataFrame, save_path: str) -> None:
     Saves a dataframe to parquet - ensures as we loop through combinations of namespaces that the parquet is added to instead of overwritten.
     """
     try:
-        dataframe.to_parquet(save_path, overwrite=True)  # Overwrite existing file
+        dataframe.to_parquet(save_path)  # Overwrites existing file
         print("File saved successfully.")
-    except FileExistsError:
-        print(f"File already exists at '{save_path}'. Use overwrite=True to replace it.")
+
 
 def combine_parquet(out_path: str) -> None:
     """
